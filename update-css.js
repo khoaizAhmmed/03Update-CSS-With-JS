d87bae2d71d5fd75de0257004f50455f3878e003
khoaiz Ahmmed Tusher <60465549+khoaizahmmed@users.noreply.github.com>
2020-05-08 23:45:06 +0600

    Add files via upload

---
+++ update-css.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll('.controls input')
+
+function updateHandle(){
+    const suffix =this.dataset.sizing || ''
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix )
+    
+}
+
+inputs.forEach(input => input.addEventListener('change' , updateHandle))
+inputs.forEach(input => input.addEventListener('click' , updateHandle))
